12f8b772423bc6462db9b2c651934c1199e62eb6
Aryann15 <rajlysm15@gmail.com>
2023-05-10 12:30:01 +0530

    some styling and input tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
-import { Box, VStack, HStack,Stack, Heading, Button} from '@chakra-ui/react'
-
+import { Input, Box, VStack, HStack,Stack, Heading, Button} from '@chakra-ui/react'
+import {AiOutlineSend} from 'react-icons/ai'
 
 const Footer = () => {
   return (
     <Box bgColor={"blackAlpha.600"} minH={'40'} p='11' color={'white'}>
         <Stack direction ={['column', 'row']}>
-            <VStack >
-                <Heading size='md' >HELLo wORLD</Heading>
-                <HStack>
-                    <Button>
+            <VStack alignItems={'stretch'}>
+                <Heading size='md' >SUBSCRIBE FOR UPDATES</Heading>
+                <HStack borderBottom={'2px solid white'} py='2'>
+                    <Input placeholder="Enter email Here...."
+                    border={'none'}
+                    borderRadius="none"
+                    outline={'none'}
+                    focusBorderColor='none'
+                    />
+                    
+                    <Button
+                    p={'0'}
+                    colorScheme="telegram" 
+                    variant={'ghost'}                  >
+                    
+                    <AiOutlineSend size={20} />
+                    
                     </Button>
                 </HStack>
             </VStack>
+
+<VStack w={"full"}
+borderLeft={['none',"1px solid white"]}
+borderRight={['none',"1px solid white"]}>
+
+</VStack>
+
         </Stack>
 
     </Box>

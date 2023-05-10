404781a4904815c5c0a6230d047cd6c145904d0c
Aryann15 <rajlysm15@gmail.com>
2023-05-10 16:44:32 +0530

    copied content form login page as s=almost same

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
-
-const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
-
-export default Signup
+import {
+    Container,
+    VStack,
+    Heading,
+    Input,
+    Button,
+    Text,
+  } from '@chakra-ui/react';
+  import React from 'react';
+  import { Link } from 'react-router-dom';
+  const Signup = () => {
+    return (
+      <Container maxW={'container.xl'} h={'100vh'} p={'16'}>
+        <form>
+          <VStack
+            alignItems={'stretch'}
+            spacing={'8'}
+            w={['full','96']}
+            m={'auto'}
+            my={'16'}
+          >
+            <Heading>WELCOME BACK!</Heading>
+  
+            <Input
+              placeholder={'email'}
+              type={'email'}
+              required
+              focusBorderColor={'blue.500'}
+            />
+            <Input
+              placeholder={'password'}
+              type={'password'}
+              required
+              focusBorderColor={'blue.500'}
+            />
+            <Button variant={'link'} alignSelf={'flex-end'}>
+              <Link to={'/forgetpassword'}>Forget Password</Link>
+            </Button>
+  
+            <Button colorScheme={'telegram'} type={'submit'}>
+              {' '}
+              Login
+            </Button>
+            <Text textAlign={'right'}> New User?{" "}
+            <Button variant={'link'} colorScheme={'telegram'}>
+              <Link to={'/signup'}>Sign Up</Link>
+            </Button>
+            </Text>
+          </VStack>
+        </form>
+      </Container>
+    );
+  };
+  
+  export default Signup;
+  

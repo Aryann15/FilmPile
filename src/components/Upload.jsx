a479d3f799f2d7dfb9107586330785aaf1832401
Aryann15 <rajlysm15@gmail.com>
2023-05-10 16:03:31 +0530

    using a upload icon for uploadinf videos

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Container, VStack } from '@chakra-ui/react';
+import React from 'react';
+import { AiOutlineUpload } from 'react-icons/ai';
 
 const Upload = () => {
   return (
-    <div>Upload</div>
-  )
-}
+    <Container maxW={'container.xl'} h={'100vh'} p={'16'}>
+      <VStack color={'blue.600'} h={'full'} justifyContent={'center'}>
+        <AiOutlineUpload size={'10vmax'} />
+      </VStack>
+    </Container>
+  );
+};
 
-export default Upload
+export default Upload;

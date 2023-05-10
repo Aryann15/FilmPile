474b26082768606d5cfc54ae71b8fc141b46f96b
Aryann15 <rajlysm15@gmail.com>
2023-05-10 13:08:26 +0530

    footer component over

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Input, Box, VStack, HStack,Stack, Heading, Button} from '@chakra-ui/react'
+import {Flex,Text, Image, Input, Box, VStack, HStack,Stack, Heading, Button} from '@chakra-ui/react'
 import {AiOutlineSend} from 'react-icons/ai'
+import img2 from '../assets/2.png';
 
 const Footer = () => {
   return (
-    <Box bgColor={"blackAlpha.600"} minH={'40'} p='11' color={'white'}>
+    <Box bgColor={"blackAlpha.600"} minH={'10'} p='11' color={'white'}>
         <Stack direction ={['column', 'row']}>
-            <VStack alignItems={'stretch'}>
-                <Heading size='md' >SUBSCRIBE FOR UPDATES</Heading>
+            <VStack w={'full'}
+>
+                <Heading  >SUBSCRIBE FOR UPDATES</Heading>
                 <HStack borderBottom={'2px solid white'} py='2'>
                     <Input placeholder="Enter email Here...."
                     border={'none'}
@@ -30,8 +32,37 @@ const Footer = () => {
 <VStack w={"full"}
 borderLeft={['none',"1px solid white"]}
 borderRight={['none',"1px solid white"]}>
-
+<Box>
+      <Flex  justifyContent="space-evenly">
+        <Heading textAlign="center">FILMPILE   </Heading>
+        <Image src={img2} w={'20'} ml={'7'} rounded="full" />
+        
+      </Flex>
+      
+    </Box>
+    <Text py={'7'}>
+All rights received
+        </Text>
 </VStack>
+<VStack
+w={'full'}>
+    <Heading size={'md'}>
+        SOCIAL MEDIA
+    </Heading>
+    <Button variant={'link'} colorScheme="telegram"
+    variant="ghost">
+        <a target={'blank'} href="https://github.com/Aryann15">Github</a>
+    </Button>
+    <Button variant={'link'} colorScheme="telegram"
+    variant="ghost">
+        <a target={'blank'} href="https://www.instagram.com/wannabe_schrute/">Instagram</a>
+    </Button>
+    <Button variant={'link'} colorScheme="telegram"
+    variant="ghost">
+        <a target={'blank'} href="https://twitter.com/RajArya17884633">Twitter</a>
+    </Button>
+</VStack>
+
 
         </Stack>
 

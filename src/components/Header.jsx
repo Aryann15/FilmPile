5ebaf678c3d2b885e91834a007fc9c3853032889
Aryann15 <rajlysm15@gmail.com>
2023-05-10 09:56:38 +0530

    added abutton with

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,27 +5,51 @@ import {
   DrawerHeader,
   DrawerOverlay,
   DrawerContent,
+  VStack,
   DrawerCloseButton,
   Button,
+  useDisclosure,
+  Stack,
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { BiMenuAltLeft } from 'react-icons/bi';
 
 const Header = () => {
+
+const {isOpen,onOpen,onClose} = useDisclosure();
+
   return (
     <>
       <Button
         pos={'fixed'}
         top={'4'}
         left={'4'}
-        colorScheme="cyan"
+        colorScheme="telegram"
         p={'0'}
         h={'10'}
         w={'10'}
         borderRadius={'full'}
+        onClick={onOpen}
       >
         <BiMenuAltLeft size={'20'} />
       </Button>
+
+      <Drawer isOpen={isOpen} placement='left' onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>MY VIDEOS</DrawerHeader>
+            <DrawerBody>
+                <VStack>
+                    <Button colorScheme="telegram">
+                        <Link to={"/"}>Home</Link>
+                    </Button>
+                </VStack>
+            </DrawerBody>
+            <div>hello</div>
+        </DrawerContent>
+
+      </Drawer>
     </>
   );
 };

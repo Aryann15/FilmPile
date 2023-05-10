7e3a67342eb19838071c977b74bbcc819ab3171f
Aryann15 <rajlysm15@gmail.com>
2023-05-10 16:42:04 +0530

    login oage completed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,55 @@
-import { Container, VStack, Heading ,Input} from '@chakra-ui/react'
-import React from 'react'
-
+import {
+  Container,
+  VStack,
+  Heading,
+  Input,
+  Button,
+  Text,
+} from '@chakra-ui/react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 const Login = () => {
   return (
     <Container maxW={'container.xl'} h={'100vh'} p={'16'}>
-    
-    <form>
-        <VStack alignItems={"stretch"} spacing={'8'} w={'96'} m={'auto'} my={'16'}>
-           <Heading>WELCOME BACK!</Heading> 
+      <form>
+        <VStack
+          alignItems={'stretch'}
+          spacing={'8'}
+          w={['full','96']}
+          m={'auto'}
+          my={'16'}
+        >
+          <Heading>WELCOME BACK!</Heading>
 
-           <Input placeholder={'email'} type={'email'} required focusBorderColor={"blue.500"} />     
-          </VStack>
-     </form>
+          <Input
+            placeholder={'email'}
+            type={'email'}
+            required
+            focusBorderColor={'blue.500'}
+          />
+          <Input
+            placeholder={'password'}
+            type={'password'}
+            required
+            focusBorderColor={'blue.500'}
+          />
+          <Button variant={'link'} alignSelf={'flex-end'}>
+            <Link to={'/forgetpassword'}>Forget Password</Link>
+          </Button>
+
+          <Button colorScheme={'telegram'} type={'submit'}>
+            {' '}
+            Login
+          </Button>
+          <Text textAlign={'right'}> New User?{" "}
+          <Button variant={'link'} colorScheme={'telegram'}>
+            <Link to={'/signup'}>Sign Up</Link>
+          </Button>
+          </Text>
+        </VStack>
+      </form>
     </Container>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;

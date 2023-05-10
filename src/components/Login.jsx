104e52df9e73ddf80da35dabf009ae2ab80bf113
Aryann15 <rajlysm15@gmail.com>
2023-05-10 16:31:57 +0530

    input tag initialied with style

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,18 @@
+import { Container, VStack, Heading ,Input} from '@chakra-ui/react'
 import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Container maxW={'container.xl'} h={'100vh'} p={'16'}>
+    
+    <form>
+        <VStack alignItems={"stretch"} spacing={'8'} w={'96'} m={'auto'} my={'16'}>
+           <Heading>WELCOME BACK!</Heading> 
+
+           <Input placeholder={'email'} type={'email'} required focusBorderColor={"blue.500"} />     
+          </VStack>
+     </form>
+    </Container>
   )
 }
 

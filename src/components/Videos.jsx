f2479fbd2d874cce361e48168dfc6f4547b31067
Aryann15 <rajlysm15@gmail.com>
2023-05-10 15:54:09 +0530

    assets imported

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,6 +1,14 @@
 import { Stack, VStack, Heading, Text, Button } from '@chakra-ui/react';
 import React, { useState } from 'react';
 
+import vid1 from '../assets/3.mp4';
+import vid2 from '../assets/4.mp4';
+import vid3 from '../assets/5.mp4';
+import vid4 from '../assets/6.mp4';
+
+import vid6 from '../assets/8.mp4';
+import vid7 from '../assets/9.mp4';
+import vid8 from '../assets/10.mp4';
 
 const Videos = () => {
   const videosArr = [vid1, vid2, vid3, vid4, vid6, vid7, vid8,];

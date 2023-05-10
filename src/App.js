43fc6f269a95a4692ee7d27e2f65ecef13dc1a71
Aryann15 <rajlysm15@gmail.com>
2023-05-10 16:19:16 +0530

    app page after utilising all components ad pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Button } from '@chakra-ui/react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Videos from './components/Videos';
+import Upload from './components/Upload';
 function App() {
   return(
     <Router>
-      <Button colorScheme={"cyan"}>Click</Button>
+      <Header />
       <Routes>
-        <Route />
+        <Route path='/' element={<Home />}/>
+        <Route path='/videos' element={<Videos />}/>
+        <Route path='/upload' element={<Upload />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<Signup />}/>
       </Routes>
+      <Footer />
     </Router>
   )
 }
